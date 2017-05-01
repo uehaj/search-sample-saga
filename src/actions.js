fde2e0cdc385a680c34fd083103ecb6fbac9f710
UEHARA Junji <uehaj@jggug.org>
2017-05-01 12:17:13 +0900

    add actions

---
+++ src/actions.js
@@ -0,0 +1,11 @@
+import { createAction } from 'redux-actions';
+
+// action.js
+export const CHANGE_INPUT = 'CHANGE_INPUT';
+export const LOAD_RESULT = 'LOAD_RESULT';
+
+// キー入力変更時
+export const refresh = createAction(CHANGE_INPUT);
+
+// API結果取得時
+export const loadResult = createAction(LOAD_RESULT);
